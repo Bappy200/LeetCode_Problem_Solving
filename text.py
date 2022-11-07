d8a674f4b96309b72369adca8aaa2c64674dc021
Bappy200 <sbappyi200@gmail.com>
2022-11-07 12:19:10 +0600

    lengthOfLastWord problem solve

--- text.py
+++ text.py
@@ -1,36 +1,39 @@
 # Iterative Binary Search Function
 # It returns index of x in given array arr if present,
 # else returns -1
-def binary_search(arr, x):
-	low = 0
-	high = len(arr) - 1
-	mid = 0
+# def binary_search(arr, x):
+# 	low = 0
+# 	high = len(arr) - 1
+# 	mid = 0
+#
+# 	while low <= high:
+# 		mid = (high + low) // 2
+#
+#
+# 		if arr[mid] < x:
+# 			low = mid + 1
+#
+# 		# If x is smaller, ignore right half
+# 		elif arr[mid] > x:
+# 			high = mid - 1
+#
+# 		# means x is present at mid
+# 		else:print(high, low, mid); return mid
+#
+#
+#
+# 	# If we reach here, then the element was not present
+# 	print(high, low, mid); return mid
+#
+#
+# # Test array
+# arr = [ 2, 3, 4, 10, 40 ]
+# x = 51
+#
+# # Function call
+# result = binary_search(arr, x)
+#
+# print(result)
 
-	while low <= high:
-		mid = (high + low) // 2
-
-
-		if arr[mid] < x:
-			low = mid + 1
-
-		# If x is smaller, ignore right half
-		elif arr[mid] > x:
-			high = mid - 1
-
-		# means x is present at mid
-		else:print(high, low, mid); return mid
-
-
-
-	# If we reach here, then the element was not present
-	print(high, low, mid); return mid
-
-
-# Test array
-arr = [ 2, 3, 4, 10, 40 ]
-x = 51
-
-# Function call
-result = binary_search(arr, x)
-
-print(result)
+s = "   fly me   to   the moon  "
+print(s.split().pop())
