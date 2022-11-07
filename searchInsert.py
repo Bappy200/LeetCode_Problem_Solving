48224d280e2fc5c77c71b89d5a3aff5e88877cf2
Bappy200 <sbappyi200@gmail.com>
2022-11-07 12:00:10 +0600

    searchInsert problem solve

---
+++ searchInsert.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
--- text.py
+++ text.py
@@ -1,4 +1,36 @@
+# Iterative Binary Search Function
+# It returns index of x in given array arr if present,
+# else returns -1
+def binary_search(arr, x):
+	low = 0
+	high = len(arr) - 1
+	mid = 0
 
-a = ["(","file"]
+	while low <= high:
+		mid = (high + low) // 2
 
-print("(" in a[0])
+
+		if arr[mid] < x:
+			low = mid + 1
+
+		# If x is smaller, ignore right half
+		elif arr[mid] > x:
+			high = mid - 1
+
+		# means x is present at mid
+		else:print(high, low, mid); return mid
+
+
+
+	# If we reach here, then the element was not present
+	print(high, low, mid); return mid
+
+
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 51
+
+# Function call
+result = binary_search(arr, x)
+
+print(result)
